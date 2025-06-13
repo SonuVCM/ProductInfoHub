0f69cd165b3ea753496b4a29e3162d3228358915
Sonu Kumar <sonuchandravanshi6201@gmail.com>
2025-06-13 08:06:16 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import  jsonData  from './data/products.json'; 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProductInfoHub';
+  products: any[] = jsonData.products;
 }
